bf43f1822b700e0eb030728cf8344b51133db940
thanos <thanos.papapetrou@gmail.com>
2016-06-30 23:24:57 +0300

    Simplified parser and added parsing exceptions

--- src/main/java/com/github/thanospapapetrou/funcky/FunckyException.java
+++ src/main/java/com/github/thanospapapetrou/funcky/FunckyException.java
@@ -2,9 +2,24 @@ package com.github.thanospapapetrou.funcky;
 
 import javax.script.ScriptException;
 
+/**
+ * Exception thrown when a parsing or runtime error occurs.
+ * 
+ * @author thanos
+ */
 public abstract class FunckyException extends ScriptException {
 	private static final long serialVersionUID = 1L;
 
+	/**
+	 * Construct a new Funcky exception.
+	 * 
+	 * @param message
+	 *            the message of the exception
+	 * @param fileName
+	 *            the name of the file in which the error occurred
+	 * @param lineNumber
+	 *            the line of the file in which the error occurred
+	 */
 	public FunckyException(final String message, final String fileName, int lineNumber) {
 		super(message, fileName, lineNumber);
 	}
