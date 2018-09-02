75a39f30f671edc7da63684cdf591660d2e97f83
thanos <thanos.papapetrou@gmail.com>
2018-09-02 19:54:40 +0300

    Starting from scratch

---
+++ src/main/java/com/github/thanospapapetrou/funcky/FunckyEngineFactory.java
@@ -0,0 +1,86 @@
+package com.github.thanospapapetrou.funcky;
+
+import java.util.List;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineFactory;
+
+/**
+ * Class implementing a Funcky engine factory.
+ *
+ * @author thanos
+ */
+public class FunckyEngineFactory implements ScriptEngineFactory {
+    @Override
+    public String getEngineName() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String getEngineVersion() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public List<String> getExtensions() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String getLanguageName() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String getLanguageVersion() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String getMethodCallSyntax(final String object, final String method,
+            final String... arguments) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public List<String> getMimeTypes() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public List<String> getNames() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String getOutputStatement(final String message) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Object getParameter(final String key) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String getProgram(final String... statements) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public ScriptEngine getScriptEngine() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+}
